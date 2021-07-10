89d3320053f7e11fb32167e46264b777a1071ef6
jolizaro <jolizaro@gmail.com>
2021-07-10 13:16:13 -0400

    heroku config

--- server/server.js
+++ server/server.js
@@ -25,7 +25,10 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/client/build" )))
+app.get("*", (req,res)=> res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')))
+}
 
 // Start the API server
 app.listen(PORT, function() {
